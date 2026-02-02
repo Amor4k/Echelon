0cb4ac1dcef56f67caf731d7fb4b96b6e0b87401
esayin <esayin@student.ethz.ch>
2026-02-02 21:57:12 +0100

    Initial commit with rough structure and start with parsing logic.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Amor4k/Echelon
+
+go 1.25.6
---
+++ internal/parser/parser.go
@@ -0,0 +1,20 @@
+package parser
+
+type LogEntry struct {
+	Timestamp  string                 `json:"ts"`
+	Category   string                 `json:"cat"`
+	Message    string                 `json:"msg"`
+	Data       interface{}            `json:"data"`
+	WorldState map[string]interface{} `json:"w-state"`
+	ID         int                    `json:"id"`
+	Version
+}
+
+//Filters logs by given ckey. Returns slice of LogEntry and error if any.
+func FilterByCkey(filename string, ckey string) ([]LogEntry, error) {
+	//TODO: Open log files.
+	//TODO: Go through each line, parse JSON, check if ckey matches. If yes, append to result slice.
+	// Return result slice and error if any.
+
+	return nil, nil
+}
